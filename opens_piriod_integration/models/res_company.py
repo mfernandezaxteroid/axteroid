3b0dbcc05ba49bbbcaac44880bd75aeaf47cc0d3
bbarraza-axteroid <bbarraza@axteroid.com>
2024-12-10 17:05:21 -0300

    Delete opens_piriod_integration directory
    Branco Barraza

--- opens_piriod_integration/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    organization_id = fields.Char(string='ID Organización Piriod')
-    token = fields.Char(string='Token Piriod')
-    piriod_connection_url = fields.Char(string='URL API Piriod')
