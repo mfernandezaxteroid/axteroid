d04e6dd9314236fb2ea961915f3e3e8c144bde40
bbarraza-axteroid <bbarraza@axteroid.com>
2025-09-09 20:50:41 -0300

    Update __manifest__.py

--- cl_bice_nomina_export/__manifest__.py
+++ cl_bice_nomina_export/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'summary': 'Exportación de nómina proveedores Banco BICE desde pagos por lote',
     'category': 'Accounting',
-    'author': 'BrancoBarraza',
+    'author': 'Branco Barraza',
     'depends': ['account_batch_payment'],
     'data': [
         'security/ir.model.access.csv',
