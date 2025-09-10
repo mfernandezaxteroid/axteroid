c2d9a5a215c2d9019078fe66056d794239a08417
bbarraza-axteroid <bbarraza@axteroid.com>
2025-09-10 09:40:17 -0300

    Update bice_export.py (codificacion-latin)

--- cl_bice_nomina_export/models/bice_export.py
+++ cl_bice_nomina_export/models/bice_export.py
@@ -92,7 +92,7 @@ class AccountBatchPayment(models.Model):
         # Ensamblar CSV sin encabezado, con \r\n
         output = "\r\n".join(export_lines) + "\r\n"
         filename = f"{datetime.now().strftime('%Y%m%d')}_Proveedores.csv"
-        export_file = base64.b64encode(output.encode('windows-1252'))
+        export_file = base64.b64encode(output.encode('latin-1'))
 
         wizard = self.env['bice.export.wizard'].create({
             'file_data': export_file,
