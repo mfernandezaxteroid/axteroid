d765af3101ee5aca65d605c3c20df09b2b86e011
bbarraza-axteroid <bbarraza@axteroid.com>
2025-09-09 23:07:31 -0300

    Update bice_export.py (ref_invoice)
    # Obtener número de documento desde la(s) factura(s) conciliada(s)
    invoice = payment.reconciled_invoice_ids[:1]  # Tomar solo la primera si hay varias
    doc_number = invoice.l10n_latam_document_number if invoice else ''
    ref = (doc_number or '').ljust(15)

--- cl_bice_nomina_export/models/bice_export.py
+++ cl_bice_nomina_export/models/bice_export.py
@@ -63,8 +63,15 @@ class AccountBatchPayment(models.Model):
             office_destiny = '1'
 
             # Referencia (factura, etc): hasta 15 caracteres
-            ref = (payment.ref or '').strip()[:15].ljust(15)
+        #   ref = (payment.ref or '').strip()[:15].ljust(15)
+        #
+        # Obtener número de documento desde la(s) factura(s) conciliada(s)
+        invoice = payment.reconciled_invoice_ids[:1]  # Tomar solo la primera si hay varias
+        doc_number = invoice.l10n_latam_document_number if invoice else ''
+        ref = (doc_number or '').ljust(15)
 
+
+            
             # Email (opcional): hasta 50 caracteres
             email = (partner.email or '').strip()[:50].ljust(50)
 
